78787ac1890b09c2fefc7d857321ffe88ed3581e
Adan Ruiz <ardn0001@hotmail.com>
2024-10-14 10:13:27 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,14 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class GoogleTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         self.addCleanup(self.browser.quit)
 
     def test_page_title(self):
