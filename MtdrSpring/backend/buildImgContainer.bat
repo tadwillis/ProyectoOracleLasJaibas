811fa212372eaf1364ff834b96c8beba4c65a809
Adan Ruiz <ardn0001@hotmail.com>
2025-03-05 10:59:20 -0600

    fix

--- MtdrSpring/backend/buildImgContainer.bat
+++ MtdrSpring/backend/buildImgContainer.bat
@@ -2,5 +2,5 @@ docker stop agilecontainer
 docker rm -f agilecontainer
 docker rmi agileimage
 call mvn verify
-docker build -f Dockerfile --platform linux/amd64 -t agileimage:0.1 DockerfileDev
+docker build -f DockerfileDev --platform linux/amd64 -t agileimage:0.1 .
 docker run --name agilecontainer -p 8080:8080 -d agileimage:0.1
